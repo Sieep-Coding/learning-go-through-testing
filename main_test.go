c41e680d0f25234eb24d71bc2989bf26720ceec4
Sieep-Coding <nstamb6@gmail.com>
2024-09-29 21:23:48 -0400

    refactor function in testing

--- main_test.go
+++ main_test.go
@@ -6,15 +6,18 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Nick")
 		want := "Hello, Nick"
-		if got != want {
-			t.Errorf("Got %q, want %q", got, want)
-		}
+		AssertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is applied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		AssertCorrectMessage(t, got, want)
 	})
+}
+
+func AssertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
