9a9485a6a196fe38bce707093100dbf4f284dd91
Sieep-Coding <nstamb6@gmail.com>
2024-09-30 08:17:42 -0400

    why?

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	t.Run("Collection of 5 numbers", func(t *testing.T) {
+	t.Run("Collection of 5 numbers!", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 		got := Sum(numbers)
 		want := 15
