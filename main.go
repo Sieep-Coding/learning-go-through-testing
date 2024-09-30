c478cfc1ace218e23cbf91d66a87a8bdc850463b
Sieep-Coding <nstamb6@gmail.com>
2024-09-29 21:38:18 -0400

    fix

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 )
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
--- main_test.go
+++ main_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Nick")
+		got := Hello("Nick", "English")
 		want := "Hello, Nick"
 		AssertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is applied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 		AssertCorrectMessage(t, got, want)
 	})
