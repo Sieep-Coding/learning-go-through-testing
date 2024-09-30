791fbf879e289d0d9c89682d3335ef6137df3582
Sieep-Coding <nstamb6@gmail.com>
2024-09-29 21:19:22 -0400

    more testing

--- main_test.go
+++ main_test.go
@@ -3,9 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Nick")
-	want := "Hello, Nick"
-	if got != want {
-		t.Errorf("Got %q, want %q", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Nick")
+		want := "Hello, Nick"
+		if got != want {
+			t.Errorf("Got %q, want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is applied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
