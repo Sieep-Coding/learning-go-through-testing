3831980b40ea624a444740c54845b95c865fbb61
Sieep-Coding <nickstambaugh@proton.me>
2024-10-31 11:49:34 -0400

    more array tests

---
+++ Structs, methods, & interfaces/shapes_test.go
@@ -0,0 +1,3 @@
+func (t *testing.T) {
+	
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -25,11 +25,25 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t *testing.T) {
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %d, want %d", got, want)
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
 	}
+	t.Run("sum 2 slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum more than 3 slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{1, 4, 2}, []int{5, 2, 1}, []int{})
+		want := []int{6, 7, 8, 0}
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum different array sizes")
 }
 
 func TestSumAllTails(t *testing.T) {
@@ -47,11 +61,6 @@ func TestSumAllTails(t *testing.T) {
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-		checkSums(t, got, want)
-	})
-	t.Run("sum more than 3 slices", func(t *testing.T) {
-		got := SumAll([]int{1, 2, 3}, []int{1, 4, 2}, []int{5, 2, 1}, []int{})
-		want := []int{6, 7, 8, 0}
 		checkSums(t, got, want)
 	})
 }
